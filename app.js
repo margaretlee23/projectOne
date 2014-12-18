9e156c0f1e00413c6edc7f891bd53c3b0496b0e1
Margaret Lee <Margaret.Mi.Lee@gmail.com>
2014-12-18 10:14:14 -0800

    Updates for thursday

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var express 	 = require('express'),
 	path		 = require('path'),
 	bodyParser 	 = require('body-parser'),
 	cookieParser = require('cookie-parser'),
-	session    	 = require('express-session');
+	session    	 = require('express-session'),
 	db			 = require('./db.js'),
 	methodOverride = require('method-override');
 
@@ -14,7 +14,7 @@ var LocalStrategy = require('passport-local').Strategy
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-// app.use('/images', express.static(__dirname + 'images'))
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({'extended':true}));
@@ -22,7 +22,8 @@ app.use(session({
   secret: 'keyboard cat',
   resave: false,
   saveUninitialized: true
-}))
+}));
+app.use(methodOverride('_method'));
 
 var localStrategy = new LocalStrategy({
   usernameField: 'email'
@@ -37,7 +38,7 @@ var localStrategy = new LocalStrategy({
 
 
       if (err) { return done(err); }
-      if (!user) { return done(null, false, { message: 'Unknown user ' + username }); }
+      if (!user) { return done(null, false, { message: 'Unknown user'}); }
       if (user.password != password) { return done(null, false, { message: 'Invalid password' }); }
       return done(null, user);
     })
@@ -68,7 +69,7 @@ app.get('/', function(req, res) {
 });
 
 app.post('/search', function(req, res) {
-	db.query('SELECT * FROM users u JOIN address a ON a.user_id = u.id JOIN category c ON c.user_id = u.id WHERE c.category = $1 and a.city = $2', [req.body.category, req.body.city], function(err, dbRes) {
+	db.query('SELECT * FROM users u JOIN category c ON c.user_id = u.id WHERE c.category = $1 and u.city = $2', [req.body.category, req.body.city], function(err, dbRes) {
 		if (!err) {
 				console.log(dbRes.rows);
 				res.render('result', { users: dbRes.rows });
@@ -87,13 +88,6 @@ app.get('/users/new', function(req, res) {
 });
 
 
-
-app.post('/sessions', function(req, res) {
-	req.logout();
-	res.redirect('/');
-});
-
-
 app.post('/users', function(req, res) {
 	db.query('INSERT INTO users (name, email, password, address, city, category) VALUES ($1, $2, $3, $4, $5, $6)', [req.body.name, req.body.email, req.body.password, req.body.address, req.body.city, req.body.category], function(err, dbRes) {
 			if (!err) {
@@ -102,10 +96,12 @@ app.post('/users', function(req, res) {
 	});
 });
 
+
+
+
 app.get('/sessions/new', function(req, res) {
 	res.render('sessions/new');
 });
-
 
 app.post('/sessions', passport.authenticate('local', 
   {failureRedirect: '/sessions/new'}), function(req, res) {
@@ -113,8 +109,10 @@ app.post('/sessions', passport.authenticate('local',
     res.redirect('/profiles/' + id);
 });
 
-
-
+app.delete('/sessions', function(req, res) {
+	req.logout();
+	res.redirect('/');
+});
 
 // app.get('/profiles', function(req, res) {
 // 	db.query('SELECT * FROM users', function(err, dbRes) {
@@ -127,46 +125,41 @@ app.get('/profiles/:id', function(req, res) {
 		if (!err) {
 			res.render('profiles/show', { user: dbRes.rows[0],
 			                              authUser: req.user });
-		}
+		} 
 	});
 });
-
-
-
-
-
-
 
   // confirm that :id is the same as req.user.id
   // next();
   // else redirect to login
 
 app.get('/profiles/:id/edit', function(req, res) {
-	res.send(req.params.id)
-	// db.query('SELECT * FROM users WHERE id = $1', [req.params.id], function(err, dbRes) {
-	// 	if (!err) {
-	// 		res.render('profiles/edit', { users: dbRes.rows[0] });
-	// 	}
-	// });
-});
-
-// app.patch('/profiles/:id', function(req, res) {
-// 	db.query('UPDATE profiles SET title = $1, body = $2 WHERE id = $3', [req.body.title, req.body.body, req.params.id], function(err, dbRes) {
-// 		if (!err) {
-// 			res.redirect('/profiles/' + req.params.id);
-// 		}
-// 	});
-// });
-
-
-app.delete('/profiles/:id', function(req, res) {
-	db.query('DELETE FROM profiles WHERE id = $1', [req.params.id], function(err, dbRes) {
-		if (!err) {
-			res.redirect('/profiles');
+	db.query('SELECT * FROM users WHERE id = $1', [req.params.id], function(err, dbRes) {
+		if (!err && (req.params.id == req.user.id)) {
+			res.render('profiles/edit', { user: dbRes.rows[0],
+			                              authUser: req.user });
+		} else {
+			res.redirect('/sessions/new');
 		}
-	})
+	});
 });
 
+app.patch('/profiles/:id', function(req, res) {
+	db.query('UPDATE users SET name = $1, email = $2, password = $3 WHERE id = $4', [req.body.name, req.body.email, req.body.password, req.params.id], function(err, dbRes) {
+		if (!err) {
+			res.redirect('/profiles/' + req.params.id);
+		}
+	});
+});
+
+
+// app.delete('/profiles/:id', function(req, res) {
+// 	db.query('DELETE FROM users WHERE id = $1', [req.params.id], function(err, dbRes) {
+// 		if (!err) {
+// 			res.redirect('/profiles');
+// 		}
+// 	})
+// });
 
 
 app.listen(8080, function() {
